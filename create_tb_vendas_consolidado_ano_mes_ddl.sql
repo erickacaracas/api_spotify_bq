37469dfabbdd9ba0d3130322eae9b55c54ab60c9
Éricka Caracas <61212337+erickacaracas@users.noreply.github.com>
2024-10-07 09:09:57 -0300

    Update nome

--- create_tb_vendas_consolidado_ano_mes_ddl.sql
+++ create_tb_vendas_consolidado_ano_mes_ddl.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TABLE `data-engineering-3254.trusted_vendas.tb_vendas_consolidado_ano_mes_` (
+CREATE OR REPLACE TABLE `data-engineering-3254.trusted_vendas.tb_vendas_consolidado_ano_mes` (
   ANO INTEGER,
   MES INTEGER,
   TOTAL_VENDAS INTEGER
